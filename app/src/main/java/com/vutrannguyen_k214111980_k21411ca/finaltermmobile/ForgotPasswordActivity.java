aa53c4673df2f6c8feb23a03f268a57097d3d901
levietduong <duonglv21411ca@st.uel.edu.vn>
2024-05-03 01:32:13 +0700

    add ForgotPasswordActivity

---
+++ app/src/main/java/com/vutrannguyen_k214111980_k21411ca/finaltermmobile/ForgotPasswordActivity.java
@@ -0,0 +1,19 @@
+package com.vutrannguyen_k214111980_k21411ca.finaltermmobile;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class ForgotPasswordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+}
